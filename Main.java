690bd5c0451202e864932a1abb163f509eee7d2a
ANDREYEMY1992 <lupuandrei1992@yahoo.com>
2015-01-20 22:32:48 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package ProblemaPentalog;
+
+public class Main {
+
+    public static void main(String[] args) {
+	NumerePrime p1 = new NumerePrime();
+	p1.metodacitire();
+	p1.metodaPrim();
+    }
+}
